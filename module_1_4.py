ba4ccda343173b198b45c3db8551d965897f59f2
Samvel <tradeup.education@gmail.com>
2024-08-01 18:09:21 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Для продолжения нажмите Enter")
+print("Количество символов в введенном тексте:",len(my_string))
+print("my_string". upper())
+print("my_string". lower())
+print("my_string". replace("_",""))
+print("my_string"[0])
+print("my_string"[-1])
+
